9757ecce3e7861fa8e6339d4f6171a9a51aaef6d
khanak0509 <kkhanak512@gmail.com>
2026-04-03 15:33:32 +0530

    SECRET_KEY load from .env

--- backend/helpers/jwt.py
+++ backend/helpers/jwt.py
@@ -1,8 +1,14 @@
 import os
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+from dotenv import load_dotenv
 
-SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY environment variable is not set")
+
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
 
